31f55f3051508095acc07abc11fd0c0900fd1320
Air-Sergej <rsv1907@yandex.ru>
2022-05-02 15:03:21 +0300

    Добавил функции и тесты!

---
+++ src/main/kotlin/Message.kt
@@ -0,0 +1,2 @@
+data class Message (val senderId: Int, val id: Int, var text: String, var date: Long, var read: Boolean) {
+}
---
+++ src/test/kotlin/ChatTest.kt
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ChatTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @Test
+    fun addMessage() {
+    }
+
+    @Test
+    fun editMessage() {
+    }
+
+    @Test
+    fun removeMessage() {
+    }
+
+    @Test
+    fun isRead() {
+    }
+
+    @Test
+    fun getFriendId() {
+    }
+}
